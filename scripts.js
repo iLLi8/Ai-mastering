020f0e8e20be8dc77c9b1406990b275cdf74bd57
iLLi8 <118803795+iLLi8@users.noreply.github.com>
2024-11-21 13:23:12 +0000

    Add files via upload
    Ai mastering project

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById("upload-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Your track has been uploaded. Please proceed to payment to download the mastered version!");
+  });
+  
